00fd4815a541f0ffbbfeecd03fd8b2365ac47a9c
Erudition <Erudition@users.noreply.github.com>
2023-05-06 01:03:47 +0000

    ci: deploy preview for Erudition/Minder/branch/master
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- Erudition/Minder/branch/master/assets/web-b9eff002.js
+++
@@ -1,2 +0,0 @@
-import{W as n}from"./index-092a5a91.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
-//# sourceMappingURL=web-b9eff002.js.map
---
+++ Erudition/Minder/branch/master/assets/web-f1e5da69.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index-f3955275.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-f1e5da69.js.map
