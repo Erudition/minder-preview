39e7beb3196a27aa4be8a30ef2446246b9d9273b
Erudition <Erudition@users.noreply.github.com>
2023-05-13 22:45:47 +0000

    ci: deploy preview for Erudition/Minder/branch/master
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

---
+++ Erudition/Minder/branch/master/assets/web-614a5dbb.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index-18e8a79e.js";class s extends n{async show(e){if(typeof document<"u"){let a=2e3;e.duration&&(a=e.duration==="long"?3500:2e3);const t=document.createElement("pwa-toast");t.duration=a,t.message=e.text,document.body.appendChild(t)}}}export{s as ToastWeb};
+//# sourceMappingURL=web-614a5dbb.js.map
