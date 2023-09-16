2ed293c9c7123be2ecc36105cfb3224b30af25d5
Erudition <Erudition@users.noreply.github.com>
2023-09-16 22:14:30 +0000

    ci: deploy preview for Erudition/Minder/branch/master
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- Erudition/Minder/branch/master/assets/status-tap-0fc2c5e6.js
+++
@@ -1,4 +0,0 @@
-import{b as a,f as i,d,w as c,s as l}from"./index-79f4d0f8.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>d(t,r)).then(()=>{c(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
-//# sourceMappingURL=status-tap-0fc2c5e6.js.map
