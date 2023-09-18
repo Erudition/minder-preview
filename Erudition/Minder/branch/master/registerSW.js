ffb35aae0ce5962eaed46b787adeaabcb646c559
Erudition <Erudition@users.noreply.github.com>
2023-09-18 08:15:57 +0000

    ci: deploy preview for Erudition/Minder/branch/master
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- Erudition/Minder/branch/master/registerSW.js
+++ Erudition/Minder/branch/master/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/minder-preview/Erudition/Minder/branch/master/sw2.js', { scope: '/minder-preview/Erudition/Minder/branch/master/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/minder-preview/Erudition/Minder/branch/master/sw.js', { scope: '/minder-preview/Erudition/Minder/branch/master/' })})}
