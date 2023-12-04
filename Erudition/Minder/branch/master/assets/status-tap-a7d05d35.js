f1229749314640fe2ccd955f4846a177ef2104c9
Erudition <Erudition@users.noreply.github.com>
2023-12-04 17:41:52 +0000

    ci: deploy preview for Erudition/Minder/branch/master
    Commit created with [EndBug/pages-preview](https://github.com/EndBug/pages-preview)

--- Erudition/Minder/branch/master/assets/status-tap-a7d05d35.js
+++
@@ -1,4 +0,0 @@
-import{i,j as a,k as c,w as d,s as l}from"./index-c5fe85df.js";/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{i(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=a(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
-//# sourceMappingURL=status-tap-a7d05d35.js.map
---
+++ Erudition/Minder/branch/master/assets/web-8094edd4.js
@@ -0,0 +1,2 @@
+import{W as n}from"./index-55038d19.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
+//# sourceMappingURL=web-8094edd4.js.map
